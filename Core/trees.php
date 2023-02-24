3f84d636e8e04a7057eb814de34703a26000ff6d
AAlexandrovic <92386024+AAlexandrovic@users.noreply.github.com>
2023-02-24 11:36:39 +0300

    Create trees.php

---
+++ Core/trees.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'apples' => [
+        rand(40,50),
+        rand(40,50),
+        rand(40,50),
+        rand(40,50),
+        rand(40,50),
+        rand(40,50),
+        rand(40,50),
+        rand(40,50),
+        rand(40,50),
+        rand(40,50),
+    ],
+    'pears' => [
+        rand(0,20),
+        rand(0,20),
+        rand(0,20),
+        rand(0,20),
+        rand(0,20),
+        rand(0,20),
+        rand(0,20),
+        rand(0,20),
+        rand(0,20),
+        rand(0,20),
+        rand(0,20),
+        rand(0,20),
+        rand(0,20),
+        rand(0,20),
+        rand(0,20),
+    ]
+];
